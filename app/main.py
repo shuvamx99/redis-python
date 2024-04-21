511d4119f1d2eca56020c2e28cd4e4462635a225
shuvam-yugen <shuvam.pandey@yugen.ai>
2024-04-21 13:35:13 +0530

    4th stage

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
-
-def handle_connection(client_socket):
+def handle_client(client_socket):
     with client_socket:
         while True:
             recv = client_socket.recv(1024)
@@ -18,8 +18,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, addr = server_socket.accept() # wait for client
-    handle_connection(client_socket=client_socket)
+    print("Server is waiting for Client to connect ...")
+    
+    while True:
+        client_socket, addr = server_socket.accept() # wait for client
+        print(f"Connection made with {addr}")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
 
 if __name__ == "__main__":
     main()
